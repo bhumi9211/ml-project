8bcf68aaeb770fcca582579431263b40cd4dc1d2
Bhumi Singla <bhumi321a@gmail.com>
2026-07-28 23:08:47 +0530

    prediction made

--- application.py
+++ application.py
@@ -71,5 +71,10 @@ def predict_datapoint():
         )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
